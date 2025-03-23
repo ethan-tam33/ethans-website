7c2234d74c9a53403a4af1a9eb0ae42515e09037
Ethan Tam <ethantam@berkeley.edu>
2025-03-22 17:16:09 -0700

    padding

--- ethans-portfolio/app/classes/page.tsx
+++ ethans-portfolio/app/classes/page.tsx
@@ -1,7 +1,7 @@
 export default function Classes() {
     return (
         <section id="home" className="animate-fadeIn animation-delay-2 selection:bg-blue-100 dark:selection:bg-blue-500 pt-12">
-            <div className="flex flex-col text-center items-center justify-center my-6 py-10 sm:py-20 md:py-20 md:flex-row md:space-x-4 md:text-left">
+            <div className="flex flex-col text-center items-center justify-center my-6 py-10 sm:py-20 md:py-20 md:flex-row md:space-x-4 md:text-left md:pl-20">
                 <div className="md:mt-0 animate-fadeIn animation-delay-4">
                     <h1 className="font-sans text-3xl font-bold mt-4 md:mt-0 md:text-5xl pb-4">
                         Classes
