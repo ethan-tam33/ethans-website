ae79367e5296f5460a650f570f7e249e9fb9358a
Ethan Tam <ethantam@berkeley.edu>
2025-03-28 15:15:49 -0700

    added experience

---
+++ ethans-portfolio/app/experience/page.tsx
@@ -0,0 +1,41 @@
+export default function Experience() {
+    return (
+        <section id="home" className="animate-fadeIn animation-delay-2 selection:bg-blue-100 dark:selection:bg-blue-500 pt-12">
+            <div className="flex flex-col text-center items-center justify-center my-6 py-10 sm:py-20 md:py-20 md:flex-row md:space-x-4 md:text-left">
+                <div className="md:mt-0 md:w-3/5 animate-fadeIn animation-delay-4">
+                    <h1 className="font-sans text-3xl font-bold mt-4 md:mt-0 md:text-5xl pb-4">
+                        Experience
+                    </h1>
+
+                    <div className="my-6 font-mono">
+                        <span className="font-bold">Uber</span>
+                        <br></br>
+                        Software Engineering Intern, Summer 2025
+
+                        <br></br>
+                        <br></br>
+
+                        <span className="font-bold">Lawrence Berkeley National Lab</span>
+                        <br></br>
+                        Machine Learning Undergraduate Research Intern, Fall 2024 + Spring 2025
+
+                        <br></br>
+                        <br></br>
+
+                        <span className="font-bold">Uber</span>
+                        <br></br>
+                        UberSTAR Software Engineering Intern, Summer 2024
+
+                        <br></br>
+                        <br></br>
+
+                        <span className="font-bold">Janelia Research Campus</span>
+                        <br></br>
+                        Machine Learning Undergraduate Research Intern, Summer 2023
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
+
--- ethans-portfolio/components/Navbar.tsx
+++ ethans-portfolio/components/Navbar.tsx
@@ -4,8 +4,8 @@ import Tab from './Tab';
 export default function Navbar() {
     return (
         <div className='flex items-center justify-end space-x-8 p-4 fixed top-0 right-5 w-full h-16 selection:bg-blue-100 dark:selection:bg-blue-500'>
-
             <Tab href="/" text="Home"></Tab>
+            <Tab href="/experience" text="Experience"></Tab>
             <Tab href="/projects" text="Projects"></Tab>
             <Tab href="/classes" text="Classes"></Tab>
             <ThemeToggle></ThemeToggle>
