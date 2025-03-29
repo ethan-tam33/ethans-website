e30786e8fb3ad15037fa8a75371e0eca5312c850
Ethan Tam <ethantam@berkeley.edu>
2025-03-29 02:16:40 -0700

    fixed navbar

--- ethans-portfolio/components/Tab.tsx
+++ ethans-portfolio/components/Tab.tsx
@@ -7,7 +7,7 @@ export default function Tab({href, text}: {href: string, text: string}) {
         <div className="mt-4 flex justify-end selection:bg-blue-100 dark:selection:bg-blue-500  ">
             <Link 
                 href={href}
-                className="relative text-slate-700 group font-mono dark:text-white"
+                className="relative text-black group font-mono dark:text-white"
             >
                 {text}
                 <span className="absolute left-0 bottom-0 w-0 h-0.5 bg-slate-700 transition-all duration-300 group-hover:w-full dark:bg-white"></span>
