051d67cff8c350e1af99d192ca74e05590f5921f
Ethan Tam <ethantam@berkeley.edu>
2025-09-12 20:48:49 -0700

    refactored classes

---
+++ ethans-portfolio/app/classes/classes.tsx
@@ -0,0 +1,60 @@
+export const classes = [
+  {
+    semester: "Fall 2025",
+    courses: [
+      "CS 161: Computer Security",
+      "CS 162: Operating Systems and System Programming",
+      "CS 194-196: Agentic AI",
+    ],
+  },
+  {
+    semester: "Spring 2025",
+    courses: [
+      "CS 170: Efficient Algorithms and Intractable Problems",
+      "CS 182: Designing, Visualizing and Understanding Deep Neural Networks",
+      "CS 168: Introduction to the Internet: Architecture and Protocols",
+      "CMPBIO 198BC: Berkeley Connect in Computational Biology (Seminar)",
+    ],
+  },
+  {
+    semester: "Fall 2024",
+    courses: [
+      "CS 189: Introduction to Machine Learning",
+      "CS 180: Introduction to Computer Vision and Computational Photography",
+      "DATA 140: Probability for Data Science",
+    ],
+  },
+  {
+    semester: "Spring 2024",
+    courses: [
+      "CS 61C: Great Ideas in Computer Architecture",
+      "EECS 127: Optimization Models in Engineering",
+      "DATA 104: Human Contexts and Ethics of Data",
+    ],
+  },
+  {
+    semester: "Fall 2023",
+    courses: [
+      "CS 70: Discrete Mathematics and Probability Theory",
+      "MATH 54: Linear Algebra and Differential Equations",
+      "DATA 100: Principles and Techniques of Data Science",
+    ],
+  },
+  {
+    semester: "Spring 2023",
+    courses: [
+      "CS 61B: Data Structures",
+      "CS 198-099: Introduction to Full-Stack Development",
+      "MATH 53: Multivariable Calculus",
+      "STAT 33B: Introduction to Advanced Programming in R",
+    ],
+  },
+  {
+    semester: "Fall 2022",
+    courses: [
+      "CS 61A: Structure and Interpretation of Computer Programs",
+      "DATA 8: Foundations of Data Science",
+      "MATH 1B: Calculus 2",
+    ],
+  },
+];
--- ethans-portfolio/app/classes/page.tsx
+++ ethans-portfolio/app/classes/page.tsx
@@ -1,78 +1,31 @@
+import { classes } from "./classes";
+
 export default function Classes() {
-    return (
-        <section id="home" className="animate-fadeIn animation-delay-2 selection:bg-blue-100 dark:selection:bg-blue-500 pt-12">
-            <div className="flex flex-col text-center items-center justify-center my-6 py-10 sm:py-20 md:py-20 md:flex-row md:space-x-4 md:text-left md:pl-20">
-                <div className="md:mt-0 animate-fadeIn animation-delay-4">
-                    <h1 className="font-sans text-3xl font-bold mt-4 md:mt-0 md:text-5xl pb-4">
-                        Classes
-                    </h1>
+  return (
+    <section
+      id="home"
+      className="animate-fadeIn animation-delay-2 selection:bg-blue-100 dark:selection:bg-blue-500 pt-12"
+    >
+      <div className="flex flex-col text-center items-center justify-center my-6 py-10 sm:py-20 md:py-20 md:flex-row md:space-x-4 md:text-left md:pl-20">
+        <div className="md:mt-0 animate-fadeIn animation-delay-4">
+          <h1 className="font-sans text-3xl font-bold mt-4 md:mt-0 md:text-5xl pb-4">
+            Classes
+          </h1>
 
-                    <div className="my-6 font-mono">
-                        <span className="font-bold">Fall 2025</span>
-                        <ul className="list-disc pl-5">
-                            <li>CS 161: Computer Security</li>
-                            <li>CS 162: Operating Systems and System Programming</li>
-                            <li>CS 194-196: Agentic AI</li>
-                        </ul>
-
-                        <br></br>
-
-                        <span className="font-bold">Spring 2025</span>
-                        <ul className="list-disc pl-5">
-                            <li>CS 170: Efficient Algorithms and Intractable Problems</li>
-                            <li>CS 182: Designing, Visualizing and Understanding Deep Neural Networks</li>
-                            <li>CS 168: Introduction to the Internet: Architecture and Protocols</li>
-                            <li>CMPBIO 198BC: Berkeley Connect in Computational Biology (Seminar)</li>
-                        </ul>
-
-                        <br></br>
-
-                        <span className="font-bold">Fall 2024</span>
-                        <ul className="list-disc pl-5">
-                            <li>CS 189: Introduction to Machine Learning</li>
-                            <li>CS 180: Introduction to Computer Vision and Computational Photography</li>
-                            <li>DATA 140: Probability for Data Science</li>
-                        </ul>
-
-                        <br></br>
-
-                        <span className="font-bold">Spring 2024</span>
-                        <ul className="list-disc pl-5">
-                            <li>CS 61C: Great Ideas in Computer Architecture</li>
-                            <li>EECS 127: Optimization Models in Engineering</li>
-                            <li>DATA 104: Human Contexts and Ethics of Data</li>
-                        </ul>
-
-                        <br></br>
-
-                        <span className="font-bold">Fall 2023</span>
-                        <ul className="list-disc pl-5">
-                            <li>CS 70: Discrete Mathematics and Probability Theory</li>
-                            <li>MATH 54: Linear Algebra and Differential Equations</li>
-                            <li>DATA 100: Principles and Techniques of Data Science</li>
-                        </ul>
-
-                        <br></br>
-
-                        <span className="font-bold">Spring 2023</span>
-                        <ul className="list-disc pl-5">
-                            <li>CS 61B: Data Structures</li>
-                            <li>CS 198-099: Introduction to Full-Stack Development</li>
-                            <li>MATH 53: Multivariable Calculus</li>
-                            <li>STAT 33B: Introduction to Advanced Programming in R</li>
-                        </ul>
-
-                        <br></br>
-
-                        <span className="font-bold">Fall 2022</span>
-                        <ul className="list-disc pl-5">
-                            <li>CS 61A: Structure and Interpretation of Computer Programs</li>
-                            <li>DATA 8: Foundations of Data Science</li>
-                            <li>MATH 1B: Calculus 2</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </section>
-    );
+          <div className="my-6 font-mono">
+            {classes.map(({ semester, courses }) => (
+              <div key={semester} className="mb-6">
+                <span className="font-bold">{semester}</span>
+                <ul className="list-disc pl-5">
+                  {courses.map((course) => (
+                    <li key={course}>{course}</li>
+                  ))}
+                </ul>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
